623817327a4f29a473dd7469e57b88227060f5b9
ztouch <41793244+ztouch@users.noreply.github.com>
2021-02-09 22:35:13 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+/*[side nav bar]
+==========================================================*/
+
+function openNav() 
+    {
+    document.getElementById("myNav").style.width = "300px";
+    }
+
+function closeNav() 
+    {
+    document.getElementById("myNav").style.width = "0%";
+    }
+
+    
